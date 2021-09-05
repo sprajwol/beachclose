9eebee76f90ff60ae786472d154f86118a840b5b
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2021-09-05 13:24:28 +0545

    project initialization and settings customization

---
+++ beachclose/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for beachclose project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beachclose.settings')
+
+application = get_asgi_application()
