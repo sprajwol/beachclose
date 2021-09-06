00661dfe3119d800e2775166d4bd3c72853653a1
Prajwol Man Shakya <shakya.prajwol@outlook.com>
2021-09-06 14:56:13 +0545

    footer page urls fixed; member and testimonials from backend done.

--- about/views.py
+++ about/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 
+from about.models import Member
 # Create your views here.
+
 
 class AboutView(TemplateView):
     template_name = 'about/about.html'
@@ -9,6 +11,9 @@ class AboutView(TemplateView):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
 
+        members = Member.objects.all()
+
+        context['members_data'] = members
         context['about_page'] = 'current-menu-item'
 
         return context
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 
+from about.models import Testimonial
 # Create your views here.
+
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
@@ -9,6 +11,9 @@ class HomeView(TemplateView):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
 
+        testimonials = Testimonial.objects.all()
+
+        context['testimonials_data'] = testimonials
         context['home_page'] = 'current-menu-item'
 
         return context
